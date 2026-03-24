f40dda548a23032fd7d99d656a61e9fc0e529ba0
sjfprojects <salvador.fairley1@gmail.com>
2026-03-24 19:12:27 +1100

    working create list button

--- src/script.js
+++ src/script.js
@@ -2,10 +2,33 @@ function changePage(newPage) {
     document.getElementById("page-title").innerHTML = newPage;
 }
 
-function createNewList(listName) {
-    console.log("creating list" + listName);
-    const list = document.createElement("button");
-    list.textContent = listName;
-    list.onclick = changePage(listName);
-    document.getElementById("custom-lists").appendChild(list);
+function createNewList() {
+    const input = document.createElement("input");
+    input.type = "text";
+    document.getElementById("list-container").prepend(input);
+    input.focus();
+    document.getElementById("new-list-button").disabled = true;
+
+    input.addEventListener("blur", (e) => {
+        input.remove();
+        document.getElementById("new-list-button").disabled = false;
+    })
+
+    input.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+            const value = input.value;
+            input.remove();
+
+            const listButton = document.createElement("button");
+            listButton.textContent = value;
+            listButton.addEventListener("click", () => {
+                changePage(value);
+            });
+
+            document.getElementById("list-container").append(listButton);
+            document.getElementById("new-list-button").disabled = false;
+
+            changePage(value);
+        }
+    });
 }
