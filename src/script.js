0bdcc71c53faf4580fbf5bcbea01a3dc0bc85cbe
sjfprojects <salvador.fairley1@gmail.com>
2026-03-24 13:01:23 +1100

    sort of working create list button

--- src/script.js
+++ src/script.js
@@ -2,6 +2,10 @@ function changePage(newPage) {
     document.getElementById("page-title").innerHTML = newPage;
 }
 
-function createNewList() {
-    
+function createNewList(listName) {
+    console.log("creating list" + listName);
+    const list = document.createElement("button");
+    list.textContent = listName;
+    list.onclick = changePage(listName);
+    document.getElementById("custom-lists").appendChild(list);
 }
